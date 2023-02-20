2be1749943e0f34720262c9fcdb182c0c27336e5
José Domingo Muñoz <josedom24@gmail.com>
2023-02-20 18:49:41 +0100

    new version

---
+++ app.py
@@ -0,0 +1,21 @@
+from time import sleep
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hola, mundo</h1>"
+
+@app.route("/healthz")
+def healthz():
+    sleep(2)
+    return "ok"
+
+@app.route("/ready")
+def ready():
+    sleep(5)
+    return "ok"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
